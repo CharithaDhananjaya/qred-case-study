71791a6d317a3fdbed45d83605f3544e1e0f7676
CharithaDhananjaya <charitha.dhananjaya464@gmail.com>
2026-06-06 18:40:51 +0200

    📦 chore : scaffold backend app with TypeScript, Express, and Lambda stubs

---
+++ apps/backend/src/handler.ts
@@ -0,0 +1,5 @@
+export const dashboardHandler = async () => {}
+export const transactionsHandler = async () => {}
+export const invoiceHandler = async () => {}
+export const getCardHandler = async () => {}
+export const activateCardHandler = async () => {}
